5cdd216a270d764b2db192556265442bbe3b6b46
NazarPetrakov <nazarpetrakov1@gmail.com>
2025-11-05 17:57:17 +0200

    Create mvvm avalonia app

---
+++ AmazonMusicParser/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AmazonMusicParser.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _greeting = "Welcome";
+}
