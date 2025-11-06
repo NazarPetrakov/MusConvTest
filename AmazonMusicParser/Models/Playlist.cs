0b3a98f403cc6109867d18dc7bf61e74c11751e9
NazarPetrakov <nazarpetrakov1@gmail.com>
2025-11-06 10:26:45 +0200

    Fix image displaying

--- AmazonMusicParser/Models/Playlist.cs
+++ AmazonMusicParser/Models/Playlist.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
+using Avalonia.Media.Imaging;
 
 namespace AmazonMusicParser.Models;
 
 public class Playlist
 {
     public string Name { get; set; } = "";
-    public Uri? AvatarUrl { get; set; }
+    public Bitmap? Avatar { get; set; }
     public string Description { get; set; } = "";
     public List<Song> Songs { get; set; } = new();
 }
